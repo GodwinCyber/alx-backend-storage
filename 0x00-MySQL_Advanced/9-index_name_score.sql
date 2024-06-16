b515ec02ffda86081ab751209530a7af1ce6a057
Godwin <godwinchuks032@gmail.com>
2024-06-16 03:30:25 +0000

    Optimize simple score

---
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -0,0 +1,3 @@
+-- Creates an index idx_name_first_score on the table names and
+-- Only the first letter of name AND score must be indexed
+CREATE INDEX idx_name_first_score ON names(name(1), score);
