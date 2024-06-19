2955d928c59576b627bec9e707f13aab53e4eba5
Godwin <godwinchuks032@gmail.com>
2024-06-19 22:59:29 +0100

    Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,8 +3,25 @@
 
 import redis
 import uuid
-from typing import Union, Callable, Optional
+from typing import Union, Any, Callable
+from functools import wraps
 
+
+def count_calls(method: Callable) -> Callable:
+    """count how many times methods of the Cache class are called.
+        count_calls decorator that takes a single method Callable 
+        argument and returns a Callable.
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs) -> Any:
+        """wrapper function that count method
+            key, use the qualified name of method using
+            the __qualname__ dunder method.
+        """
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 class Cache:
     """Create a Cache class. In the __init__ method, store an
@@ -14,7 +31,7 @@ class Cache:
     def __init__(self):
         """Initializing cache instance"""
         self._redis = redis.Redis()
-        self._redis.flushdb()
+        self._redis.flushdb(True)
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """create  store method that take a data argunment and return a string
@@ -26,7 +43,7 @@ class Cache:
         self._redis.set(key, data)
         return key
     
-    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
         """create get method that takes a key and an optional [Callable] fn argument
            The Callable will be used to convert data back to the desired format
         Arg:
@@ -39,11 +56,11 @@ class Cache:
         data = self._redis.get(key)
         if data is None:
             return None
-        if fn:
+        if fn is not None:
             return fn(data)
         return data
 
-    def get_str(self, key: str) -> Optional[str]:
+    def get_str(self, key: str) -> str:
         """Retrieve data as a stirng from Redis
         Arg:
             key: The key of the data to retrieve
@@ -52,7 +69,7 @@ class Cache:
         """
         return self.get(key, lambda x: x.decode('utf-8'))
 
-    def get_int(self, key: str) -> Optional[int]:
+    def get_int(self, key: str) -> int:
         """Retrieve data as an integer from Redis
         Args:
             key: The key of the data to retrieve
